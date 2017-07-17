7deac94c87281ecffc5495fb9275262e1a4f57b0
Michael Kapelko <kornerr@gmail.com>
2017-07-17 05:55:59 -0700

    Introduce 'lastMessage' Variable for SendView

--- sample-chat/SendView/SendView.swift
+++ sample-chat/SendView/SendView.swift
@@ -1,11 +1,43 @@
 
+import RxCocoa
+import RxSwift
 import UIKit
 
 class SendView: UIView {
 
-    // MARK PRIVATE
+    // MARK: - PUBLIC
 
+    let lastMessage: Variable<String> = Variable("")
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.setupSendView()
+    }
+
+    // MARK: - PRIVATE
+    
+    private let disposeBag: DisposeBag = DisposeBag()
+
+    @IBOutlet private var textField: UITextField!
     @IBOutlet private var sendButton: UIButton!
+
+    private func setupSendView() {
+        NSLog("SendView.setup")
+
+        // Report latest message.
+        self.sendButton.rx.tap.subscribe(onNext: { [unowned self] _ in
+            if (self.textField != nil) {
+              self.lastMessage.value = self.textField!.text!
+            }
+        })
+        .disposed(by: disposeBag)
+
+        // Erase text field.
+        self.sendButton.rx.tap.subscribe(onNext: { [unowned self] _ in
+            self.textField?.text = ""
+        })
+        .disposed(by: disposeBag)
+    }
 
 }
 
