dbf5cb2282865b49b1817cf11bda3a0ebd259bb6
Michael Kapelko <kornerr@gmail.com>
2017-07-17 00:56:42 -0700

    Display stub cells

--- sample-chat/Chats/ChatsVC/ChatsVC.swift
+++ sample-chat/Chats/ChatsVC/ChatsVC.swift
@@ -3,7 +3,7 @@ import RxCocoa
 import RxSwift
 import UIKit
 
-class ChatsVC : UIViewController {
+class ChatsVC : UIViewController, UITableViewDataSource {
 
     // MARK: PUBLIC
 
@@ -33,8 +33,33 @@ class ChatsVC : UIViewController {
             .bind(to: self.deviceTokenTextView.rx.text)
             .disposed(by: disposeBag)
         */
+        let cellNib = UINib(nibName: "ChatsCell", bundle: nil)
+        self.tableView.register(cellNib, forCellReuseIdentifier: "ChatsCell")
+        
+        self.tableView.dataSource = self
+        self.tableView.reloadData()
     }
 
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+        NSLog("ChatsVC. Number of rows")
+        return 3;
+    }
+
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        NSLog("ChatsVC. cellForRow: '\(indexPath.row)'")
+
+        let cell =
+            tableView.dequeueReusableCell(
+                withIdentifier: "ChatsCell",
+                for: indexPath)
+        NSLog("Cell: '\(cell)'")
+        return cell
+    }
     
 }
 
